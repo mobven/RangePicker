3ab81b944b0e5d9196fe28d74f2c3830b5467303
Rashid Ramazanov <rashid.ramazanov@gmail.com>
2023-06-20 15:00:56 +0300

    Update RangePickerViewTests.swift

--- Tests/RangePickerViewTests/RangePickerViewTests.swift
+++ Tests/RangePickerViewTests/RangePickerViewTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class RangePickerViewTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RangePickerView().text, "Hello, World!")
+        // TODO: tests will be added.
     }
 }
